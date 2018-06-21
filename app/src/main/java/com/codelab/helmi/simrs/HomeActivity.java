50d6637c14dcc391853ffbdc17075051189db9eb
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2018-06-21 17:57:50 +0700

    Update Bottom Navigation

--- app/src/main/java/com/codelab/helmi/simrs/HomeActivity.java
+++ app/src/main/java/com/codelab/helmi/simrs/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.codelab.helmi.simrs;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,46 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .replace(R.id.frame_container, new ChatFragment())
+                .commit();
+
+
+        BottomNavigationView bottomNavigationView =(BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        break;
+                    case R.id.action_chat:
+                        fragmentManager.beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.frame_container, new ChatFragment())
+                                .commit();
+                        break;
+
+                    case R.id.action_history:
+                        fragmentManager.beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.frame_container, new HistoryFragment())
+                                .commit();
+                        break;
+
+                    case R.id.action_info:
+                        fragmentManager.beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.frame_container, new InfoFragment())
+                                .commit();
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
     }
 }
