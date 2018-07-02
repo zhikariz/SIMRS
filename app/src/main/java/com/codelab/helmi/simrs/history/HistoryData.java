ce8eedee7741a9b6b6f8c6b01c8050e79d114e4b
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2018-07-02 10:04:17 +0700

    Nambahin slider

--- app/src/main/java/com/codelab/helmi/simrs/history/HistoryData.java
+++ app/src/main/java/com/codelab/helmi/simrs/history/HistoryData.java
@@ -1,8 +1,11 @@
 package com.codelab.helmi.simrs.history;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class HistoryData {
+public class HistoryData implements Parcelable {
     @SerializedName("id_registrasi_periksa")
     private String id_registrasi_periksa;
     @SerializedName("kategori")
@@ -133,4 +136,58 @@ public class HistoryData {
     public void setNama_poli(String nama_poli) {
         this.nama_poli = nama_poli;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id_registrasi_periksa);
+        dest.writeString(this.kategori);
+        dest.writeString(this.tanggal_pesan);
+        dest.writeString(this.create_at);
+        dest.writeString(this.no_rm);
+        dest.writeString(this.status_persetujuan);
+        dest.writeString(this.id_poli_dokter);
+        dest.writeString(this.id_asuransi);
+        dest.writeString(this.nama_asuransi);
+        dest.writeString(this.id_dokter);
+        dest.writeString(this.id_poli);
+        dest.writeString(this.nama_dokter);
+        dest.writeString(this.nama_poli);
+    }
+
+    public HistoryData() {
+    }
+
+    protected HistoryData(Parcel in) {
+        this.id_registrasi_periksa = in.readString();
+        this.kategori = in.readString();
+        this.tanggal_pesan = in.readString();
+        this.create_at = in.readString();
+        this.no_rm = in.readString();
+        this.status_persetujuan = in.readString();
+        this.id_poli_dokter = in.readString();
+        this.id_asuransi = in.readString();
+        this.nama_asuransi = in.readString();
+        this.id_dokter = in.readString();
+        this.id_poli = in.readString();
+        this.nama_dokter = in.readString();
+        this.nama_poli = in.readString();
+    }
+
+    public static final Parcelable.Creator<HistoryData> CREATOR = new Parcelable.Creator<HistoryData>() {
+        @Override
+        public HistoryData createFromParcel(Parcel source) {
+            return new HistoryData(source);
+        }
+
+        @Override
+        public HistoryData[] newArray(int size) {
+            return new HistoryData[size];
+        }
+    };
 }
